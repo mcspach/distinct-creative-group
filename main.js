d30c8c1fc3a5eb4e7e27841bf64787a51551f0ef
Matthew Spach <matthewspach@gmail.com>
2021-05-26 10:35:20 -0700

    Updates

--- main.js
+++ main.js
@@ -61,34 +61,6 @@ function checkScrollTitle() {
 window.addEventListener('scroll', checkScrollTitle, false);
 window.addEventListener('resize', checkScrollTitle, false);
 
-// second animation for titles
-
-var titles2 = document.querySelectorAll(".stretch2");
-fraction = .6;
-function checkScrollTitle2() {
-
-    for(var i = 0; i < titles2.length; i++) {
-
-        var title = titles2[i];
-
-        var x = title.offsetLeft, y = title.offsetTop, w = title.offsetWidth, h = title.offsetHeight, r = x + w, //right
-            b = y + h, //bottom
-            visibleX, visibleY, visible;
-
-            visibleX = Math.max(0, Math.min(w, window.pageXOffset + window.innerWidth - x, r - window.pageXOffset));
-            visibleY = Math.max(0, Math.min(h, window.pageYOffset + window.innerHeight - y, b - window.pageYOffset));
-
-            visible = visibleX * visibleY / (w * h);
-
-            if (visible > fraction) {
-                title.classList.add("stretching_bg");
-            }
-    }
-}
-
-window.addEventListener('scroll', checkScrollTitle2, false);
-window.addEventListener('resize', checkScrollTitle2, false);
-
 // animation for shadows on cards
 
 var cards = document.querySelectorAll(".pop_shadow");
@@ -116,4 +88,32 @@ function checkScrollCard() {
 
 window.addEventListener('scroll', checkScrollCard, false);
 window.addEventListener('resize', checkScrollCard, false);
+
+// animation for icons
+
+var icons = document.querySelectorAll(".reveal");
+fraction = .3;
+function checkScrollIcon() {
+
+    for(var i = 0; i < icons.length; i++) {
+
+        var icon = icons[i];
+
+        var x = icon.offsetLeft, y = icon.offsetTop, w = icon.offsetWidth, h = icon.offsetHeight, r = x + w, //right
+            b = y + h, //bottom
+            visibleX, visibleY, visible;
+
+            visibleX = Math.max(0, Math.min(w, window.pageXOffset + window.innerWidth - x, r - window.pageXOffset));
+            visibleY = Math.max(0, Math.min(h, window.pageYOffset + window.innerHeight - y, b - window.pageYOffset));
+
+            visible = visibleX * visibleY / (w * h);
+
+            if (visible > fraction) {
+                icon.classList.add("revealed");
+            }
+    }
+}
+
+window.addEventListener('scroll', checkScrollIcon, false);
+window.addEventListener('resize', checkScrollIcon, false);
 
