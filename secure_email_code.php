5cfc5455fb356415904caa324ebd421872f301b6
Matthew Spach <matthewspach@gmail.com>
2021-05-28 10:29:06 -0700

    Form, no submission

---
+++ secure_email_code.php
@@ -0,0 +1,30 @@
+
+<?php
+if(isset($_POST["submit"])){
+// Checking For Blank Fields..
+if($_POST["vname"]==""||$_POST["vemail"]==""||$_POST["sub"]==""||$_POST["msg"]==""){
+echo "Fill all feilds to submit.";
+}else{
+// Check if the "Sender's Email" input field is filled out
+$email=$_POST['vemail'];
+// Sanitize E-mail Address
+$email =filter_var($email, FILTER_SANITIZE_EMAIL);
+// Validate E-mail Address
+$email= filter_var($email, FILTER_VALIDATE_EMAIL);
+if (!$email){
+echo "Invalid Email Address";
+}
+else{
+$subject = $_POST['sub'];
+$message = $_POST['msg'];
+$headers = 'From:'. $email2 . "rn"; // Sender's Email
+$headers .= 'Cc:'. $email2 . "rn"; // Carbon copy to Sender
+// Message lines should not exceed 70 characters (PHP rule), so wrap it
+$message = wordwrap($message, 70);
+// Send Mail By PHP Mail Function
+mail("matthewspach@gmail.com", $subject, $message, $headers);
+echo "Thank you! We will be reaching out soon to schedule a consultation. Cant wait? Call now: 1-559-916-7165";
+}
+}
+}
+?>
